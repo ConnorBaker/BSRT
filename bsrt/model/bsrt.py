b146af757cfebb842511ecdbd35100ed4be5e538
Connor Baker <connorbaker01@gmail.com>
2022-09-20 04:13:15 +0000

    spynet: refactor into separate module

--- bsrt/model/bsrt.py
+++ bsrt/model/bsrt.py
@@ -3,25 +3,12 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import model.arch_util as arch_util
-from torch.cuda.amp import autocast
+from torch.cuda.amp.autocast_mode import autocast
 import model.swin_util as swu
-import time
-import os
-import math
-from utils.debayer import Debayer3x3
-import torchvision.utils as tvutils
-from datasets.burstsr_dataset import pack_raw_image, flatten_raw_image_batch
-
-try:
-    from model.non_local.non_local_cross_dot_product import NONLocalBlock2D as NonLocalCross
-    from model.non_local.non_local_dot_product import NONLocalBlock2D as NonLocal
-except ImportError:
-    raise ImportError('Failed to import Non_Local module.')
-
-try:
-    from model.DCNv2.dcn_v2 import DCN_sep as DCN, FlowGuidedDCN
-except ImportError:
-    raise ImportError('Failed to import DCNv2 module.')
+from spynet_util import SpyNet
+from model.non_local.non_local_cross_dot_product import NONLocalBlock2D as NonLocalCross
+from model.non_local.non_local_dot_product import NONLocalBlock2D as NonLocal
+from model.DCNv2.dcn_v2 import DCN_sep as DCN, FlowGuidedDCN
 
 
 def make_model(args, parent=False):
@@ -61,116 +48,6 @@ def make_model(args, parent=False):
                    upscale=upscale,
                    non_local=non_local,
                    use_checkpoint=use_checkpoint)
-
-
-class BasicModule(nn.Module):
-    """Basic Module for SpyNet.
-    """
-
-    def __init__(self):
-        super(BasicModule, self).__init__()
-
-        self.basic_module = nn.Sequential(
-            nn.Conv2d(in_channels=8, out_channels=32, kernel_size=7, stride=1, padding=3), nn.ReLU(inplace=False),
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=7, stride=1, padding=3), nn.ReLU(inplace=False),
-            nn.Conv2d(in_channels=64, out_channels=32, kernel_size=7, stride=1, padding=3), nn.ReLU(inplace=False),
-            nn.Conv2d(in_channels=32, out_channels=16, kernel_size=7, stride=1, padding=3), nn.ReLU(inplace=False),
-            nn.Conv2d(in_channels=16, out_channels=2, kernel_size=7, stride=1, padding=3))
-
-    def forward(self, tensor_input):
-        return self.basic_module(tensor_input)
-
-
-class SpyNet(nn.Module):
-    """SpyNet architecture.
-
-    Args:
-        load_path (str): path for pretrained SpyNet. Default: None.
-        return_levels (list[int]): return flows of different levels. Default: [5].
-    """
-
-    def __init__(self, load_path=None, return_levels=[5]):
-        super(SpyNet, self).__init__()
-        self.return_levels = return_levels
-        self.basic_module = nn.ModuleList([BasicModule() for _ in range(6)])
-        if load_path:
-            if not os.path.exists(load_path):
-                import requests
-                url = 'https://github.com/JingyunLiang/VRT/releases/download/v0.0/spynet_sintel_final-3d2a1287.pth'
-                r = requests.get(url, allow_redirects=True)
-                print(f'downloading SpyNet pretrained model from {url}')
-                os.makedirs(os.path.dirname(load_path), exist_ok=True)
-                open(load_path, 'wb').write(r.content)
-
-            self.load_state_dict(torch.load(load_path, map_location=lambda storage, loc: storage)['params'])
-
-        self.register_buffer('mean', torch.Tensor([0.485, 0.456, 0.406]).view(1, 3, 1, 1))
-        self.register_buffer('std', torch.Tensor([0.229, 0.224, 0.225]).view(1, 3, 1, 1))
-
-    def preprocess(self, tensor_input):
-        tensor_output = (tensor_input - self.mean) / self.std
-        return tensor_output
-
-    def process(self, ref, supp, w, h, w_floor, h_floor):
-        flow_list = []
-
-        ref = [self.preprocess(ref)]
-        supp = [self.preprocess(supp)]
-
-        # ref = [ref]
-        # supp = [supp]
-
-        for level in range(5):
-            ref.insert(0, F.avg_pool2d(input=ref[0], kernel_size=2, stride=2, count_include_pad=False))
-            supp.insert(0, F.avg_pool2d(input=supp[0], kernel_size=2, stride=2, count_include_pad=False))
-
-        flow = ref[0].new_zeros(
-            [ref[0].size(0), 2,
-             int(math.floor(ref[0].size(2) / 2.0)),
-             int(math.floor(ref[0].size(3) / 2.0))])
-
-        for level in range(len(ref)):
-            upsampled_flow = F.interpolate(input=flow, scale_factor=2, mode='bilinear', align_corners=True) * 2.0
-
-            if upsampled_flow.size(2) != ref[level].size(2):
-                upsampled_flow = F.pad(input=upsampled_flow, pad=[0, 0, 0, 1], mode='replicate')
-            if upsampled_flow.size(3) != ref[level].size(3):
-                upsampled_flow = F.pad(input=upsampled_flow, pad=[0, 1, 0, 0], mode='replicate')
-
-            flow = self.basic_module[level](torch.cat([
-                ref[level],
-                arch_util.flow_warp(
-                    supp[level], upsampled_flow.permute(0, 2, 3, 1), interp_mode='bilinear', padding_mode='border'),
-                upsampled_flow
-            ], 1)) + upsampled_flow
-
-            if level in self.return_levels:
-                scale = 2**(5-level) # level=5 (scale=1), level=4 (scale=2), level=3 (scale=4), level=2 (scale=8)
-                flow_out = F.interpolate(input=flow, size=(h//scale, w//scale), mode='bilinear', align_corners=False)
-                flow_out[:, 0, :, :] *= float(w//scale) / float(w_floor//scale)
-                flow_out[:, 1, :, :] *= float(h//scale) / float(h_floor//scale)
-                if torch.abs(flow_out).mean() > 200:
-                    print(f"level {level}, flow > 200: {torch.abs(flow_out).mean():.4f}")
-                    # return None
-                    flow_out.clamp(-250, 250)
-                flow_list.insert(0, flow_out)
-
-        return flow_list
-
-    def forward(self, ref, supp):
-        assert ref.size() == supp.size()
-
-        h, w = ref.size(2), ref.size(3)
-        w_floor = math.floor(math.ceil(w / 32.0) * 32.0)
-        h_floor = math.floor(math.ceil(h / 32.0) * 32.0)
-
-        ref = F.interpolate(input=ref, size=(h_floor, w_floor), mode='bilinear', align_corners=False)
-        supp = F.interpolate(input=supp, size=(h_floor, w_floor), mode='bilinear', align_corners=False)
-
-        flow_list = self.process(ref, supp, w, h, w_floor, h_floor)
-
-        return flow_list[0] if len(flow_list) == 1 else flow_list
-
 
 
 class FlowGuidedPCDAlign(nn.Module):
