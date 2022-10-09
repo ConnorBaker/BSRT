82363b34ce35982e91043f388a94c519ccd38257
Connor Baker <connorbaker01@gmail.com>
2022-10-09 14:31:00 -0400

    utility: prune unused functions

--- bsrt/utility.py
+++ bsrt/utility.py
@@ -7,84 +7,17 @@ from metrics.l1 import L1
 from metrics.l2 import L2
 from metrics.ms_ssim_loss import MSSSIMLoss
 from metrics.psnr import PSNR
-from metrics.utils.ignore_boundry import ignore_boundary
 from model.bsrt import BSRT
 from option import Config, LossName, DataTypeName
 from torch import Tensor
 from torchmetrics.metric import Metric
 from typing_extensions import Literal, get_args, overload
 from utils.postprocessing_functions import BurstSRPostProcess, SimplePostProcess
-import math
-import numpy as np
 import os
 import torch
-import torch.distributed as dist
 import torch.nn.functional as F
 import torch.optim as optim
 import torch.optim.lr_scheduler as lrs
-
-
-def reduce_mean(tensor: Tensor, nprocs: int) -> Tensor:
-    rt: Tensor = tensor.clone()  # type: ignore
-    dist.all_reduce(rt, op=dist.ReduceOp.SUM)  # type: ignore
-    rt /= nprocs
-    return rt
-
-
-def gradient(data):
-    D_dy = data[:, :, 1:] - data[:, :, :-1]
-    D_dx = data[:, :, :, 1:] - data[:, :, :, :-1]
-    return D_dx, D_dy
-
-
-def smooth_grad_1st(flo, image, alpha):
-    img_dx, img_dy = gradient(image)
-    weights_x = torch.exp(-torch.mean(torch.abs(img_dx), 1, keepdim=True) * alpha)
-    weights_y = torch.exp(-torch.mean(torch.abs(img_dy), 1, keepdim=True) * alpha)
-
-    dx, dy = gradient(flo)
-
-    loss_x = weights_x * torch.abs(dx) / 2.0
-    loss_y = weights_y * torch.abs(dy) / 2.0
-    return torch.mean(loss_x) / 2.0 + torch.mean(loss_y) / 2.0
-
-
-def smooth_loss(flow, img):
-    loss = smooth_grad_1st(flow, img, 10)
-    return sum([torch.mean(loss)])
-
-
-def cleanup():
-    dist.destroy_process_group()
-
-
-def mkdir(path: str):
-    os.makedirs(path, exist_ok=True)
-
-
-def quantize(img, rgb_range):
-    pixel_range = 255 / rgb_range
-    return img.mul(pixel_range).clamp(0, 255).round().div(pixel_range)
-
-
-def calc_psnr(sr, hr, scale, rgb_range, dataset=None):
-    if hr.nelement() == 1:
-        return 0
-
-    diff = (sr - hr) / rgb_range
-    if dataset and dataset.dataset.benchmark:
-        shave = scale
-        if diff.size(1) > 1:
-            gray_coeffs = [65.738, 129.057, 25.064]
-            convert = diff.new_tensor(gray_coeffs).view(1, 3, 1, 1) / 256
-            diff = diff.mul(convert).sum(dim=1)
-    else:
-        shave = scale + 6
-
-    valid = ignore_boundary(diff, shave)
-    mse = valid.pow(2).mean()
-
-    return -10 * math.log10(mse)
 
 
 def make_loss_fn(loss: LossName, data_type: DataTypeName) -> Metric:
@@ -241,17 +174,6 @@ def make_optimizer(config: Config, target):
     optimizer = CustomOptimizer(trainable, **kwargs_optimizer)
     optimizer._register_scheduler(scheduler_class, **kwargs_scheduler)
     return optimizer
-
-
-def write_gray_to_tfboard(img):
-    img_debug = img[0, ...].detach().cpu().numpy()
-
-    # img_debug = cv2.normalize(img_debug, None, 0, 255,
-    #                           cv2.NORM_MINMAX, cv2.CV_8U)
-    img_debug = img_debug * 255
-    img_debug = np.clip(img_debug, 0, 255)
-    img_debug = img_debug.astype(np.uint8)
-    return img_debug[0, ...]
 
 
 ######################## BayerUnifyAug ############################
