8d21ce9d14ac8a996b45e2a5ec8f4a3408c73a63
Connor Baker <connorbaker01@gmail.com>
2022-11-12 15:03:19 -0500

    spynet: Refactor

--- bsrt/model/spynet_util.py
+++ bsrt/model/spynet_util.py
@@ -1,35 +1,51 @@
 import math
+from dataclasses import dataclass, field
+from typing import ClassVar, List, Sequence, Union
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch import Tensor
 
 from bsrt.model import arch_util
 from bsrt.utils.bilinear_upsample_2d import bilinear_upsample_2d
 
 
+@dataclass(eq=False)
 class BasicModule(nn.Module):
     """Basic Module for SpyNet."""
 
-    def __init__(self):
-        super(BasicModule, self).__init__()
+    channel_steps: ClassVar[Sequence[int]] = [8, 32, 64, 32, 16, 2]
 
-        self.basic_module = nn.Sequential(
-            nn.Conv2d(in_channels=8, out_channels=32, kernel_size=7, stride=1, padding=3),
-            nn.ReLU(inplace=False),
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=7, stride=1, padding=3),
-            nn.ReLU(inplace=False),
-            nn.Conv2d(in_channels=64, out_channels=32, kernel_size=7, stride=1, padding=3),
-            nn.ReLU(inplace=False),
-            nn.Conv2d(in_channels=32, out_channels=16, kernel_size=7, stride=1, padding=3),
-            nn.ReLU(inplace=False),
-            nn.Conv2d(in_channels=16, out_channels=2, kernel_size=7, stride=1, padding=3),
+    net: nn.Sequential = field(init=False)
+
+    def __post_init__(self):
+        super().__init__()
+        self.net = nn.Sequential(
+            # We drop the first element because we do not want to apply a ReLU to the input.
+            *[
+                layer
+                for in_channels, out_channels in zip(
+                    BasicModule.channel_steps[:-1], BasicModule.channel_steps[1:]
+                )
+                for layer in [
+                    nn.LeakyReLU(inplace=False),
+                    nn.Conv2d(
+                        in_channels=in_channels,
+                        out_channels=out_channels,
+                        kernel_size=7,
+                        stride=1,
+                        padding=3,
+                    ),
+                ]
+            ][1:]
         )
 
-    def forward(self, tensor_input):
-        return self.basic_module(tensor_input)
+    def forward(self, tensor_input: Tensor) -> Tensor:
+        return self.net(tensor_input)
 
 
+@dataclass(eq=False)
 class SpyNet(nn.Module):
     """SpyNet architecture.
 
@@ -38,71 +54,76 @@ class SpyNet(nn.Module):
         return_levels (list[int]): return flows of different levels. Default: [5].
     """
 
-    def __init__(self, return_levels=[5]):
-        super(SpyNet, self).__init__()
-        self.return_levels = return_levels
-        self.basic_module = nn.ModuleList([BasicModule() for _ in range(6)])
+    weights_url: ClassVar[
+        str
+    ] = "https://github.com/JingyunLiang/VRT/releases/download/v0.0/spynet_sintel_final-3d2a1287.pth"  # noqa: E501
 
-        weights_dict = torch.hub.load_state_dict_from_url(
-            "https://github.com/JingyunLiang/VRT/releases/download/v0.0/spynet_sintel_final-3d2a1287.pth"  # noqa: E501
-        )
+    return_levels: Sequence[int] = field(default_factory=lambda: [5])
+    net: nn.ModuleList = field(init=False)
+    mean: Tensor = field(init=False)
+    std: Tensor = field(init=False)
+
+    def __post_init__(self):
+        super().__init__()
+        self.net = nn.ModuleList([BasicModule() for _ in range(6)])
+
+        weights_dict = torch.hub.load_state_dict_from_url(SpyNet.weights_url)
         self.load_state_dict(weights_dict["params"])
 
         self.register_buffer("mean", torch.Tensor([0.485, 0.456, 0.406]).view(1, 3, 1, 1))
         self.register_buffer("std", torch.Tensor([0.229, 0.224, 0.225]).view(1, 3, 1, 1))
 
-    def preprocess(self, tensor_input):
+    def preprocess(self, tensor_input: Tensor) -> Tensor:
         tensor_output = (tensor_input - self.mean) / self.std
         return tensor_output
 
-    def process(self, ref, supp, w, h, w_floor, h_floor):
-        flow_list = []
+    def process(
+        self, ref: Tensor, supp: Tensor, w: int, h: int, w_floor: int, h_floor: int
+    ) -> List[Tensor]:
+        flow_list: List[Tensor] = []
 
-        ref = [self.preprocess(ref)]
-        supp = [self.preprocess(supp)]
-
-        # ref = [ref]
-        # supp = [supp]
+        _ref: List[Tensor] = [self.preprocess(ref)]
+        _supp: List[Tensor] = [self.preprocess(supp)]
 
         # FIXME: By repeatedly averaging these values, we can end up with a tensor where the width
         # and height are one.
-        for level in range(5):
-            ref.insert(
+        for _ in range(5):
+            _ref.insert(
                 0,
-                F.avg_pool2d(input=ref[0], kernel_size=2, stride=2, count_include_pad=False),
+                F.avg_pool2d(input=_ref[0], kernel_size=2, stride=2, count_include_pad=False),
             )
-            supp.insert(
+            _supp.insert(
                 0,
-                F.avg_pool2d(input=supp[0], kernel_size=2, stride=2, count_include_pad=False),
+                F.avg_pool2d(input=_supp[0], kernel_size=2, stride=2, count_include_pad=False),
             )
 
-        flow = ref[0].new_zeros(
+        flow: Tensor = _ref[0].new_zeros(
             [
-                ref[0].size(0),
+                _ref[0].size(0),
                 2,
                 # FIXME: Continued: taking the floor of these values can yield a tensor of width
                 # or height zero.
-                ref[0].size(2) // 2,
-                ref[0].size(3) // 2,
+                _ref[0].size(2) // 2,
+                _ref[0].size(3) // 2,
             ]
         )
 
-        for level in range(len(ref)):
+        for level in range(len(_ref)):
             # FIXME: Continued: we cannot upsample a tensor with width or height zero.
             upsampled_flow = bilinear_upsample_2d(flow, scale_factor=2, align_corners=True) * 2.0
 
-            if upsampled_flow.size(2) != ref[level].size(2):
+            if upsampled_flow.size(2) != _ref[level].size(2):
                 upsampled_flow = F.pad(input=upsampled_flow, pad=[0, 0, 0, 1], mode="replicate")
-            if upsampled_flow.size(3) != ref[level].size(3):
+            if upsampled_flow.size(3) != _ref[level].size(3):
                 upsampled_flow = F.pad(input=upsampled_flow, pad=[0, 1, 0, 0], mode="replicate")
 
             flow = (
-                self.basic_module[level](
+                self.net[level](
                     torch.cat(
                         [
-                            ref[level],
+                            _ref[level],
                             arch_util.flow_warp(
-                                supp[level],
+                                _supp[level],
                                 upsampled_flow.permute(0, 2, 3, 1),
                                 padding_mode="border",
                             ),
@@ -115,7 +136,7 @@ class SpyNet(nn.Module):
             )
 
             if level in self.return_levels:
-                scale = 2 ** (
+                scale: int = 2 ** (
                     5 - level
                 )  # level=5 (scale=1), level=4 (scale=2), level=3 (scale=4), level=2 (scale=8)
                 flow_out = bilinear_upsample_2d(
@@ -131,7 +152,7 @@ class SpyNet(nn.Module):
 
         return flow_list
 
-    def forward(self, ref, supp):
+    def forward(self, ref: Tensor, supp: Tensor) -> Union[Tensor, List[Tensor]]:
         assert ref.size() == supp.size()
 
         h, w = ref.size(2), ref.size(3)
