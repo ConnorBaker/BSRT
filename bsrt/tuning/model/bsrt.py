80c16ba4ce0a26d4fefebd807a2b5f1329119ea2
Connor Baker <connorbaker01@gmail.com>
2022-11-13 03:26:39 +0000

    bsrt: Use categorical ranges for tuning some variables

--- bsrt/tuning/model/bsrt.py
+++ bsrt/tuning/model/bsrt.py
@@ -18,7 +18,9 @@ class BSRTParams:
     @classmethod
     def suggest(cls, trial: Trial) -> BSRTParams:
         # num_features must be a multiple of flow_alignmnet_groups
-        flow_alignment_groups = trial.suggest_int("flow_alignment_groups", 1, 32)
+        flow_alignment_groups: int = trial.suggest_categorical(  # type: ignore
+            "flow_alignment_groups", [2**i for i in range(2, 5)]
+        )
         num_features = trial.suggest_int(
             "num_features",
             flow_alignment_groups,
@@ -27,9 +29,11 @@ class BSRTParams:
         )
         return cls(
             attn_drop_rate=trial.suggest_float("attn_drop_rate", low=0.0, high=1.0),
-            drop_path_rate=trial.suggest_float("drop_path_rate", low=1e-2, high=1.0, log=True),
+            drop_path_rate=trial.suggest_float("drop_path_rate", low=0.0, high=1.0),
             drop_rate=trial.suggest_float("drop_rate", low=0.0, high=1.0),
-            mlp_ratio=trial.suggest_float("mlp_ratio", low=2.0, high=32.0, log=True),
+            mlp_ratio=trial.suggest_categorical(  # type: ignore
+                "mlp_ratio", [2**i for i in range(1, 6)]
+            ),
             flow_alignment_groups=flow_alignment_groups,
             num_features=num_features,
             qkv_bias=trial.suggest_categorical("qkv_bias", [True, False]),  # type: ignore
